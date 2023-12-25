baffa0c029a8d300b638dc339313e2fa6e1ac9d1
devEliasJr <eliasengmecanica@gmail.com>
2023-12-25 15:26:54 -0300

    feat: Added Footer

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import PrimarySearchAppBar from "./components/appBar/appBar";
 import SliderCarrousel from "./components/sliderCarrousel/sliderCarrousel";
 import GridProducts from "./components/gridProducts/gridProducts";
+import Footer from "./components/footer/footer";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <PrimarySearchAppBar />
       <SliderCarrousel />
       <GridProducts />
+      <Footer />
     </>
   );
 }
